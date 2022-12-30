657ba5bbc0536653b4e5c728ea2a91aa24e6b864
Yanush Viktar <vicjan89@gmail.com>
2022-12-30 17:23:40 +0300

    на базе jinja2 набраны все первичные инструктажи

--- main.py
+++ main.py
@@ -20,17 +20,67 @@ finally:
         print("Соединение с SQLite закрыто")
 
 doc = {value[2]: value[1] for value in record}
-instrOt = '\n'.join(['1. '+value[1] for value in record if value[3] and not value[4]])
-instrOtIfWork = '\n'.join(['1. '+value[1] for value in record if value[3] and value[4]])
-instrOtDriver = '\n'.join(['1. ' + value[1] for value in record if value[5] and not value[4]])
-instrOtDriverIfWork = '\n'.join(['1. ' + value[1] for value in record if value[5] and value[4]])
+instrOt = [value[1] for value in record if value[3] or value[5]]
 instrOtElectromonterLulka = [value[1] for value in record if value[3]]
 instrOtElectromonter = [value[1] for value in record if value[3] and (value[2] != 'Для люльки')]
-
+p1 = [value[1] for value in record if value[3] and (value[2] != 'Для люльки')]
 env = Environment(loader=FileSystemLoader('templates'))
-template = env.get_template('list_instr_ot.html')
-html = template.render(data=instrOtElectromonterLulka,
-                       name='Перечень инструкций по охране труда для электромонтёра по ремонту аппаратуры РЗАИ с правом работы в рабочей платформе мобильной подъёмной рабочей платформы.')
+# template = env.get_template('list_instr_ot.html')
+# html = template.render(data=instrOtElectromonterLulka,
+#                        name='Перечень инструкций по охране труда для электромонтёра по ремонту аппаратуры РЗАИ с правом работы в рабочей платформе мобильной подъёмной рабочей платформы.')
+#
+# with open('Перечень инструкций по ОТ для электромонтёра с люлькой.html', 'w') as file:
+# 	file.write(html)
+#
+# html = template.render(data=instrOtElectromonter,
+#                        name='Перечень инструкций по охране труда для электромонтёра по ремонту аппаратуры РЗАИ.')
+#
+# with open('Перечень инструкций по ОТ для электромонтёра без люльки.html', 'w') as file:
+# 	file.write(html)
+#
+# html = template.render(data=instrOt,
+#                        name='Перечень инструкций по охране труда по службе РЗАИ.')
+#
+# with open('Перечень инструкций по охране труда по службе РЗАИ.html', 'w') as file:
+# 	file.write(html)
+# template = env.get_template('progr_pervich_instrukt.html')
+# html = template.render(data=doc,
+#                        name='Программа первичного инструктажа на рабочем месте для мастера (старшего мастера) СРЗАИ.')
+#
+# with open('out\Программа первичного инструктажа на рабочем месте для мастера (старшего мастера) СРЗАИ.html', 'w') as file:
+# 	file.write(html)
+#
+# template = env.get_template('progr_pervich_instrukt_kladov.html')
+# html = template.render(data=doc,
+#                        name='Программа первичного инструктажа на рабочем месте для старшего мастера СРЗАИ с совмещением профессии кладовщика.')
+#
+# with open('out\Программа первичного инструктажа на рабочем месте для старшего мастера СРЗАИ с совмещением профессии кладовщика.html', 'w') as file:
+# 	file.write(html)
+#
+# template = env.get_template('progr_pervich_instrukt_tehnik.html')
+# html = template.render(data=doc,
+#                        name='Программа первичного инструктажа на рабочем месте для техника СРЗАИ.')
+#
+# with open('out\Программа первичного инструктажа на рабочем месте для техника СРЗАИ.html', 'w') as file:
+# 	file.write(html)
 
-with open('Перечень инструкций по ОТ.html', 'w') as file:
+# template = env.get_template('progr_pervich_instrukt_zam_nach.html')
+# html = template.render(data=doc,
+#                        name='Программа первичного инструктажа на рабочем месте для заместителя начальника СРЗАИ.')
+#
+# with open('out\Программа первичного инструктажа на рабочем месте для заместителя начальника СРЗАИ.html', 'w') as file:
+# 	file.write(html)
+#
+# template = env.get_template('progr_pervich_instrukt_ingener.html')
+# html = template.render(data=doc,
+#                        name='Программа первичного инструктажа на рабочем месте для инженера СРЗАИ.')
+#
+# with open('out\Программа первичного инструктажа на рабочем месте для инженера СРЗАИ.html', 'w') as file:
+# 	file.write(html)
+
+template = env.get_template('progr_pervich_instrukt_tehnik_metrolog.html')
+html = template.render(data=doc,
+                       name='Программа первичного инструктажа на рабочем месте для техника (по метрологии) СРЗАИ.')
+
+with open('out\Программа первичного инструктажа на рабочем месте для техника (по метрологии) СРЗАИ.html', 'w') as file:
 	file.write(html)
